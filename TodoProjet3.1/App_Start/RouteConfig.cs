6f39295232d07eb96cdb8faa52a1d49228362719
[Mahanitia] <[ramiandrizakamahanitia@gmail.com]>
2024-03-05 11:29:52 +0300

    aaa

--- TodoProjet3.1/App_Start/RouteConfig.cs
+++ TodoProjet3.1/App_Start/RouteConfig.cs
@@ -23,11 +23,6 @@ namespace TodoProjet3._1
                 name: "Tache",
                 url: "Taches/Task"
             );
-
-            routes.MapRoute(
-                name: "Tache",
-                url: "Taches/Task"
-            );
         }
     }
 }
